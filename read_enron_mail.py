1e0058d87ea6554c743922296b841a1be487b94a
mgow <mgow>
2018-03-14 18:09:30 -0500

    added sqlalchemy code - creates tables - inserts emails

--- read_enron_mail.py
+++ read_enron_mail.py
@@ -1,37 +1,105 @@
 import os
-import tarfile as t
+import os.path
 import shutil
 import re
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+from sqlalchemy import Column, Integer, String, Date, DateTime, Float
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+
+
+engine = create_engine('postgresql://mgow:postgres@localhost:5432/enron_emails', echo = True)
+meta = MetaData()
+Base = declarative_base(metadata=meta)
+
+
+class Emails(Base):
+    __tablename__ = 'emails'
+    id = Column(Integer, primary_key = True)
+    message_id = Column(String)
+    sent_datetime = Column(DateTime)
+    message_from = Column(String)
+    message_to = Column(String)
+    message_cc = Column(String)
+    message_bcc = Column(String)
+    subject = Column(String)
+    body = Column(String)
+    mime_version = Column(Float)
+    content_type = Column(String)
+    content_transfer_encoding = Column(String)
+    x_from = Column(String)
+    x_to = Column(String)
+    x_cc = Column(String)
+    x_bcc = Column(String)
+    x_folder = Column(String)
+    x_origin = Column(String)
+    x_filename = Column(String)
+
+
+class Addresses(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key = True)
+    email_id = Column(Integer, ForeignKey('emails.id'), nullable = False)
+    sender = Column(String)
+    send_type = Column(String)
+    sent_to = Column(String)
+
 
 
 #read email and return sender and message ID
-def get_metadata(file):
-    message_from = ''
-    message_id = ''
-    file = open(file)
-    #enclose in while loop to ensure metadata doesn't get overwritten by threaded emails below
-    while message_from == '' or message_id == '':
-        for line in file:
-            if line.startswith('From:'):
-                start = line.find('From:') + 60
-                end = line.find('.com') + 4
-                message_from = line[start:end]
-            elif line.startswith('Message-ID:'):
-                start = line.find('Message-ID: <') + 13
-                end = line.find('.JavaMail')
-                message_id = line[start:end]
-    return message_from, message_id
+def get_metadata(folder, filename):
+    filename = os.path.join(folder, filename)
+    pattern = r'(?s)(?:([\w-]+)[:]\s*(.*?)\s?\n)'
+    email = open(filename).read()
+    header = dict(re.findall(pattern, email))
+    return header
+
+
+emails = []
+
+for dirpath, dirnames, filenames in os.walk('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir\\arnold-j'):
+    for file in filenames:
+        emails.append(get_metadata(dirpath, file))
+
+print(emails[0])
+
+# create session
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+
+for email in emails:
+    # add rows
+    try:
+        row = Emails(message_id=email['Message-ID'], sent_datetime=email['Date'], message_from=email['From'])
+        session.add(row)
+        session.commit()
+    except SQLAlchemyError as e:
+        print(e)
+    # also need a handler for key error, as not every email has every key - we just want to skip the field if not present in the email
+    finally:
+        session.close()
+
+
+
+
+
+
+
+
+
+
 
 
 #iterate over files in from_folder and copy them all to to_folder, renaming each with message_from and message_id
 def copy_emails(from_folder, to_folder):
     for file in os.listdir(from_folder):
-        message_from, message_id = get_metadata(file)
+        message_from, message_id = get_metadata(from_folder, file)
         shutil.copy2('{}\\{}'.format(from_folder, file), '{}\\{}_{}'.format(to_folder, message_from, message_id))
-
-
-os.chdir('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir\\allen-p')
-copy_emails('sent', 'C:\\Users\\MGOW\\Documents\\PythonProject\\all_emails')
 
 
 #create new folder folder_name for each employee (useful for data cleanup)
@@ -39,39 +107,4 @@ def create_folders(folder_name):
     for folder in os.listdir('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir'):
         os.chdir('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir\\{}'.format(folder))
         os.mkdir('{}'.format(folder_name))
-
-
-#check for dupes in a folder folder_name
-def check_dupes(folder_name):
-    pass
-
-
-
-
-
-
-#sent_folders = []
-
-#for path, folders, files in os.walk('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir'):
-    #for name in folders:
-        #if name.endswith('_sent_mail'):
-            #print('path: {}, folders: {}, files: {}, count: {}'.format(path, folders, files, len(files)))
-            #sent_folders.append(os.path.join(path, name))
-        #elif name.endswith('sent'):
-            #sent_folders.append(os.path.join(path, name))
-
-#print(sent_folders)
-
-#will contain sender's name, number of emails sent, earliest sent date, latest sent date, average word count of their emails, and person they sent the most emails to
-#profiles = []
-
-#check how many sent items each person has
-#for folder in sent_folders:
-    #for path, folders, files in os.walk(folder):
-        #sent_dict = {'name': os.path.dirname(os.path.relpath(path, start='C:\\Users\\MGOW\\Documents\\PythonProject\\maildir')), 'sent_count': len(files)}
-        #profiles.append(sent_dict)
-
-#print(sorted(profiles, key = lambda k: k['sent_count'], reverse = True))  #you have to use this weird temporary lambda function to sort by the key of a dict
-
-#check the earliest sent date for a person
 
