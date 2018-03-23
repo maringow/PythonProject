33d95f6a3c6fe3382964352894b8c29eaf3df613
mgow <mgow>
2018-03-23 07:41:32 -0500

    fixed bugs: - datetime conversion error - x-bcc getting mapped to x-cc - regex issue

--- read_enron_mail.py
+++ read_enron_mail.py
@@ -1,14 +1,12 @@
 import os
 import os.path
-import shutil
 import re
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import MetaData
 from sqlalchemy import Column, Integer, String, Date, DateTime, Float
-from sqlalchemy import ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
+from dateutil import parser
 
 
 engine = create_engine('postgresql://mgow:postgres@localhost:5432/enron_emails', echo = True)
@@ -37,74 +35,84 @@ class Emails(Base):
     x_folder = Column(String)
     x_origin = Column(String)
     x_filename = Column(String)
+    label_by_key = dict(
+        message_id = 'Message-ID',
+        sent_datetime = 'Date',
+        message_from = 'From',
+        message_to = 'To',
+        message_cc = 'Cc',
+        message_bcc = 'Bcc',
+        subject = 'Subject',
+        body = 'body',
+        mime_version = 'Mime-Version',
+        content_type = 'Content-Type',
+        content_transfer_encoding = 'Content-Transfer-Encoding',
+        x_from = 'X-From',
+        x_to = 'X-To',
+        x_cc = 'X-cc',
+        x_bcc = 'X-bcc',
+        x_folder = 'X-Folder',
+        x_origin = 'X-Origin',
+        x_filename = 'X-FileName'
+    )
+    key_by_label = {label:key for key, label in label_by_key.items()}
 
 
 class Addresses(Base):
     __tablename__ = 'addresses'
     id = Column(Integer, primary_key = True)
     email_id = Column(Integer, ForeignKey('emails.id'), nullable = False)
-    sender = Column(String)
+    sender = Column(String, ForeignKey('emails.message_from'))
     send_type = Column(String)
     sent_to = Column(String)
 
 
-
-#read email and return sender and message ID
-def get_metadata(folder, filename):
+# read email and return sender and message ID
+def parse_email(folder, filename):
     filename = os.path.join(folder, filename)
-    pattern = r'(?s)(?:([\w-]+)[:]\s*(.*?)\s?\n)'
+    body_pattern = r'(?s)^(.*?)\s?\n\s?\n(.*?)(?:\s?\n\s?\nMessage-ID.*)?$'
+    header_pattern = r'(?s)(?:([\w-]+)[:]\s(.*?)\n)'
+    # header_pattern = r'(?s)(?:([\w-]+)[:]\s*(.*?)\s?\n)'
     email = open(filename).read()
-    header = dict(re.findall(pattern, email))
-    return header
-
+    try:
+        header, body = re.findall(body_pattern, email)[0]
+    except IndexError:  # figure out what we want to return here - don't want to add an empty dict to the list
+        return {}
+    parsed_email = dict(re.findall(header_pattern, header))
+    parsed_email['body'] = body
+    return parsed_email
 
 emails = []
 
-for dirpath, dirnames, filenames in os.walk('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir\\arnold-j'):
+for dirpath, dirnames, filenames in os.walk('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir'):
     for file in filenames:
-        emails.append(get_metadata(dirpath, file))
+        emails.append(parse_email(dirpath, file))
 
-print(emails[0])
 
-# create session
+# create database session
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
+key_errors = 0
 
 for email in emails:
+    try:
+        kw = {Emails.key_by_label[label]:value for label, value in email.items()}
+        dt = parser.parse(kw.pop('sent_datetime'))
+        # print(kw)
+    except KeyError as k:
+        print(k)
+        print(email)
+        key_errors += 1
     # add rows
     try:
-        row = Emails(message_id=email['Message-ID'], sent_datetime=email['Date'], message_from=email['From'])
+        row = Emails(sent_datetime=dt, **kw)
         session.add(row)
         session.commit()
     except SQLAlchemyError as e:
         print(e)
-    # also need a handler for key error, as not every email has every key - we just want to skip the field if not present in the email
     finally:
         session.close()
 
-
-
-
-
-
-
-
-
-
-
-
-#iterate over files in from_folder and copy them all to to_folder, renaming each with message_from and message_id
-def copy_emails(from_folder, to_folder):
-    for file in os.listdir(from_folder):
-        message_from, message_id = get_metadata(from_folder, file)
-        shutil.copy2('{}\\{}'.format(from_folder, file), '{}\\{}_{}'.format(to_folder, message_from, message_id))
-
-
-#create new folder folder_name for each employee (useful for data cleanup)
-def create_folders(folder_name):
-    for folder in os.listdir('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir'):
-        os.chdir('C:\\Users\\MGOW\\Documents\\PythonProject\\maildir\\{}'.format(folder))
-        os.mkdir('{}'.format(folder_name))
-
+print('Count of KeyErrors: {}'.format(key_errors))
