957d445d0f0b9c5bbac215cfef1a7923f839dbf1
mgow <mgow>
2018-05-08 10:52:42 -0500

    separated out Base (used for creating tables)

--- base.py
+++ base.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, DateTime, Float
+
 
 
 meta = MetaData()
